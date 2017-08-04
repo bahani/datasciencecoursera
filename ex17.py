6376632a8901008ca60ca968b22273b4b591a872
Bahar <bahar.ahani@yahoo.com>
2017-08-04 17:47:00 -0400

    first upload

---
+++ ex17.py
@@ -0,0 +1,22 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "copying from %s to %s" % (from_file, to_file)
+
+indata = open(from_file).read()
+
+
+print "the input file is %d bytes long" % len(indata)
+
+print "does the output file exist? %r" % exists(to_file)
+print "ready, hit return to continue, ctrl-c to abort"
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "alright, all done"
+
+out_file.close()
